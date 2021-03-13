ec4217a3bf27aaaff3a1a6b0b064af5746dab3ee
Jamie Berghmans <jamie.berghmans@student.kdg.be>
2021-03-13 19:45:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import requests
+import json
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print('Usage: python3 main.py <folder>')
+        print('Ex: python3 main.py /Users/Jamie/Desktop/icons')
+        sys.exit(1)
+
+    folderPath = sys.argv[1]
+    if not folderPath.startswith('\\') and not folderPath.startswith('/'):
+        path = os.path.abspath(os.path.dirname(__file__))
+        folderPath = os.path.join(path, folderPath)
+    
+    for (dirPath, _, fileNames) in os.walk(folderPath):
+        for fileName in fileNames:
+            name = fileName.replace('.png', '')
+            if '.' in name:
+                response = requests.get(f'https://itunes.apple.com/lookup/?bundleId={name}')
+                if response.status_code == 200:
+                    responseJson = json.loads(response.content.decode('utf-8'))
+                    if int(responseJson["resultCount"]) > 0:
+                            print(f"Renaming {fileName} to {responseJson['results'][0]['trackName'] + '.png'} in {dirPath}")
+                            os.rename(os.path.join(dirPath, fileName), os.path.join(dirPath, responseJson['results'][0]['trackName'] + '.png'))
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Requirements
+- Python 3
+# How to Install
+## Step 1:
+    Run `pip3 install -r requirements.txt` to install the required libraries.
+## Step 2:
+    Run the program.
+
+# How to run:
+    Usage: python3 main.py <folder>
+    Ex: python3 main.py /Users/Jamie/Desktop/icons
