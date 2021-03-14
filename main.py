1624a4c8f98ef77277bda1e97e72b6b626be9c74
Jamie Berghmans <jamie.berghmans@student.kdg.be>
2021-03-14 11:39:14 +0100

    Small safety check

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ if __name__ == '__main__':
     
     for (dirPath, _, fileNames) in os.walk(folderPath):
         for fileName in fileNames:
-            groups = re.match("([0-9A-z]+\.[0-9A-z]+\.[0-9A-z]+)", fileName).groups()
-            if len(groups) > 0:
-                name=groups[0]
-                response = requests.get(f'https://itunes.apple.com/lookup/?bundleId={name}')
-                if response.status_code == 200:
-                    responseJson = json.loads(response.content.decode('utf-8'))
-                    if int(responseJson["resultCount"]) > 0:
-                            print(f"Renaming {fileName} to {responseJson['results'][0]['trackName'] + '.png'} in {dirPath}")
-                            os.rename(os.path.join(dirPath, fileName), os.path.join(dirPath, responseJson['results'][0]['trackName'] + '.png'))
+            match = re.match("([0-9A-z]+\.[0-9A-z]+\.[0-9A-z]+)", fileName)
+            if match != None:
+                groups = re.match("([0-9A-z]+\.[0-9A-z]+\.[0-9A-z]+)", fileName).groups()
+                if len(groups) > 0:
+                    name=groups[0]
+                    response = requests.get(f'https://itunes.apple.com/lookup/?bundleId={name}')
+                    if response.status_code == 200:
+                        responseJson = json.loads(response.content.decode('utf-8'))
+                        if int(responseJson["resultCount"]) > 0:
+                                print(f"Renaming {fileName} to {responseJson['results'][0]['trackName'] + '.png'} in {dirPath}")
+                                os.rename(os.path.join(dirPath, fileName), os.path.join(dirPath, responseJson['results'][0]['trackName'] + '.png'))
